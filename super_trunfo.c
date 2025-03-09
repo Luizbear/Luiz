dc4c37aa6fc06d16f47df81cb4295dfcbf3bcbe9
Luizbear <luizhenrique.1004@outlook.com>
2025-03-09 18:26:32 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,5 +1,12 @@
 #intrude <stdio.h>
 
 int main(){
-
+ int Estado;;
+  char codigo da carta[300];
+  char nome da cidade[100];
+  int população;
+  float area em km²;
+  float PIB;
+  int numero de pontos turisticos;
+  
 }
