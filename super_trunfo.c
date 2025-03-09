c0d2993d7cfc82f5ec0f61a30428785f7ca9e922
Luizbear <luizhenrique.1004@outlook.com>
2025-03-09 18:44:48 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,12 +1,20 @@
 #intrude <stdio.h>
 
 int main(){
- int Estado;;
-  char codigo da carta[300];
-  char nome da cidade[100];
+ int Estado = 'A' a 'H';
+  char codigo da carta[a01, b02, c03, d04];
+  char nome da cidade[];(string);
   int população;
   float area em km²;
   float PIB;
   int numero de pontos turisticos;
   
-}
+  printf("Estado: %c\n", Estado);
+  printf("Codigo da carta: %c\n", codigo da carta);
+  printf("Nome da cidade: %s\n", nome da cidade);
+  printf("População: %d\n", população);
+  printf("Area em km²: %f\n", area em km²);
+  printf("PIB: %f\n", PIB);
+  printf("Numero de pontos turisticos: %d\n", numero de pontos turisticos);
+
+  return 0;
