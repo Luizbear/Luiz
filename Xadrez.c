f863d440b6744041c6cc1cf0e8c27f8b1fdecbd3
Luizbear <luizhenrique.1004@outlook.com>
2025-04-30 18:05:28 -0300

    Create Xadrez.c

---
+++ Xadrez.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+  //Movimentação da Rainha 8 casas
+    for (int i = 0; i < 8; i++)
+    {
+        printf("Direita\n"); //Direção do movimento
+    }
+
+    int i = 1;
+// Bispo mover 5 casas
+    do
+    {
+        printf("Direita/cima\n");
+        i++;
+
+    } while (i < 5);
+
+    //Torre mover 5 casas
+
+    while (i < 5);
+    {
+        printf("Direita\n");
+    }
+    
+    
+    
+
+    return 0;
+
+}
